283d6038d71e0912df3bc6940b01b10900e91e32
tania-abitbol <56915461+tania-abitbol@users.noreply.github.com>
2020-05-09 02:29:06 +0200

    burger menu

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Styled from "styled-components";
 import heroImage from "../images/hero-image.png";
 import nameImage from "../images/name.png";
@@ -9,6 +9,8 @@ const HeroContainers = Styled.div`
  height:100vh;
  display:flex;
  justify-content: space-between;
+
+
   `;
 export const Nav = Styled.div`
 ${rh.belowLandscapeTablet`
@@ -61,12 +63,50 @@ justify-content: space-between;
 }
 `;
 
+const BurgerMenuIcon = Styled.div`
+position:absolute;
+z-index:2;
+color:white;
+margin-left: 80%;
+`;
+
+const BurgerMenuLink = Styled.div`
+text-align:center;
+position:absolute;
+width:100%;
+background:black;
+height:100%;
+padding:50px;
+&>div{
+  margin-bottom:50%;
+}
+`;
+const LinkBurgerMenu = Styled.a`
+color:white;
+font-family: 'Montserrat', sans-serif;
+font-size:22px;
+cursor:pointer;
+text-decoration:none;
+`;
+const BurgerMenuIconBar = Styled.div`
+display:flex;
+flex-direction:column;
+margin:15px;
+
+&>div{
+  margin:4px;
+  height:2px;
+  width:25px;
+  background-color:white;
+}
+`;
 export const Hero = () => {
+  const [apparition, setApparition] = useState(false);
   return (
     <HeroContainers id="top">
       <Nav>
         <div>
-          <Link href="#whoAmI"> Qui suis-je ?</Link>
+          <Link href="#whoAmI"> Qui suis-je ?</Link>
         </div>
         <div>
           <Link href="#mySkills"> Mes compétences</Link>
@@ -78,6 +118,30 @@ export const Hero = () => {
           <Link href="#myContacts">Me contacter</Link>
         </div>
       </Nav>
+      <BurgerMenuIcon onClick={() => setApparition(!apparition)}>
+        <BurgerMenuIconBar>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+        </BurgerMenuIconBar>
+      </BurgerMenuIcon>
+      {apparition ? (
+        <BurgerMenuLink style={{ color: "white" }}>
+          <div>
+            <LinkBurgerMenu href="#whoAmI"> Qui suis-je ?</LinkBurgerMenu>
+          </div>
+          <div>
+            <LinkBurgerMenu href="#mySkills"> Mes compétences</LinkBurgerMenu>
+          </div>
+          <div>
+            <LinkBurgerMenu href="#myProjets">Mes projets</LinkBurgerMenu>
+          </div>
+          <div>
+            <LinkBurgerMenu href="#myContacts">Me contacter</LinkBurgerMenu>
+          </div>
+        </BurgerMenuLink>
+      ) : null}
+
       <HeroRightSide>
         <div>
           <img src={nameImage} alt="" />
