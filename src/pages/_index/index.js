43318fafef976c3bd3c41875da73258209572242
tania-abitbol <56915461+tania-abitbol@users.noreply.github.com>
2020-05-13 17:54:26 +0200

    texte

--- src/pages/_index/index.js
+++ src/pages/_index/index.js
@@ -19,7 +19,7 @@ export const articleData = [
     title: "Portfolio d’une etudiante en web",
     articleTitle: "Ma recherche",
     articleText:
-      "Actuellement à la recherche d’une alternance, ou à défaut d'un stage de 2 mois dans l’industrie du web.\r\n\nQuand ? En cas d'alternance, en aout 2020 pour une durée de 2 ans. Et en cas de stage, en aout 2020 pour une durée de 2 mois. Le rythme de l’alternance serait de 3 semaines en entreprise pour 1 semaine de formation.\r\n\nJe suis intéressée par l’idée de travailler dans le domaine de la programmation.\r\n\nAvant tout, je suis à la recherche d’une entreprise dans laquelle le travail d’équipe, l’entente et le respect des valeurs humaines sont centraux afin que je puisse m’épanouir tant sur le plain humain que professionnel. ",
+      "Actuellement à la recherche d’une alternance, ou à défaut d'un stage de 2 mois dans l’univers du web.\r\n\nQuand ? En cas d'alternance, en août 2020 pour une durée de 2 ans. Et en cas de stage, en août 2020 pour une durée de 2 mois. Le rythme de l’alternance serait de 3 semaines en entreprise puis 1 semaine de formation.\r\n\nJe serais vivement intéressée par l’idée de travailler dans le domaine de la programmation.\r\n\nAvant tout, je suis à la recherche d’une entreprise dans laquelle le travail d’équipe, l’entente et le respect des valeurs humaines sont centraux afin que je puisse m’épanouir tant sur le plan humain que professionnel. ",
     articleImage: eyesImage,
     blackBoard: false,
     button: false,
@@ -41,7 +41,7 @@ export const articleData = [
     title: "La créativité . . .",
     articleTitle: "Ma mentalité",
     articleText:
-      "Au cours de ma formation à HETIC, les cours de gestion d’équipe ainsi que les nombreuses semaines de projet m’ont appris à comprendre ce qu’implique le travail d’équipe. Et aussi, des projets personnel m'ont aidé à découvrir et m'ameliorer dans differents domaines de programmation.\r\n\nPar ailleurs, le respect d’autrui étant fondamental pour travailler en équipe,  l’école nous a demandé de travailler tant sur nous-même que sur les différences qui nous séparent, les différences pouvant parfois constituer une force. En m’investissant, j’en suis ressortie davantage tournée vers les autres et je remercie HETIC pour cette opportunité.\r\n\nD’un autre côté, les cours de méthode de projet m’ont permis de comprendre et d’intégrer le lexique tout en apprenant à construire méthodiquement un projet technologique.\r\n\nJ’éprouve un réel plaisir lorsque je présente ce pour quoi je travaille dur face à un public.",
+      "Au cours de ma formation à HETIC, les cours de gestion d’équipe ainsi que les nombreuses semaines de projet m’ont appris à comprendre ce qu’implique le travail d’équipe. Des projets personnels m'ont également aidée à découvrir et à m'ameliorer dans differents domaines de programmation.\r\n\nPar ailleurs, le respect d’autrui étant fondamental pour travailler en équipe, l’école nous a demandé de travailler tant sur nous-même que sur les différences qui nous séparent, celle-ci pouvant parfois constituer une force. En m’investissant, j’en suis ressortie davantage tournée vers les autres et je remercie HETIC pour cette opportunité.\r\n\nD’un autre côté, les cours de méthode de projet m’ont permis de comprendre et d’intégrer le lexique tout en apprenant à construire méthodiquement un projet technologique.\r\n\nJ’éprouve un réel plaisir lorsque je présente en public, le travail durement accompli.",
     articleImage: charactersImage,
     blackBoard: false,
     button: false,
@@ -52,7 +52,7 @@ export const articleData = [
     title: "Quelles sont mes compétences ? ",
     articleTitle: "Dans le domaine de la programmation :",
     articleText:
-      "- HTML/CSS : J’ai eu l’occasion, en plus des cours, de m’occuper de la partie intégration dans plusieurs de mes projets de groupe. Je maitrise le pré-processeur SASS et la convention de nommage des classes avec BEM.\r\n\n- JavaScript natif : Maitrise du DOM, création d’un mini jeu vidéo 100% en JS, création d’animations JS sur mes différents projets. Utilisation de différentes librairies (husky, eslint, prettier).\r\n\n- React JS : Création de ce portfolio avec ce framework, maitrise de styled-components.\r\n\n- Gatsby : Maitrise de cet environnement. Ce portfolio utilise Gatsby.\r\n\n- PHP : Un langage que j’ai beaucoup utilisé durant cette année, maitrise du design pattern MVC (model, view, controller) et donc de la programation orienté objets. Utilisation régulière du logiciel mamp et de phpmyadmin.\r\n\n- GitHub : Maitrise de cet outil que j’ai eu l’occasion d’utiliser de manière regulière.\r\n\n- Arduino : J’ai acquis de petite base car durant mon lycée mon projet au baccalauréat était de programmer une carte Arduino en C++.\r\n\n- MySql : Maitrise des actions de base.\r\n\n- Aws : Brève maitrise de l'environement aws grâce auquel j'ai eu l'occasion de deployer un projet en ligne.\r\n\n",
+      "- HTML/CSS : J’ai eu l’occasion, en plus des cours, de m’occuper de la partie intégration dans plusieurs de mes projets de groupe. Je maitrise le pré-processeur SASS et la convention de nommage des classes avec BEM.\r\n\n- JavaScript natif : Maitrise du DOM, création d’un mini jeu vidéo 100% en JS, création d’animations JS sur mes différents projets. Utilisation de différentes librairies (husky, eslint, prettier).\r\n\n- React JS : Création de ce portfolio avec ce framework, maitrise de styled-components.\r\n\n- Gatsby : Maitrise de cet environnement. Ce portfolio utilise Gatsby.\r\n\n- PHP : Un langage que j’ai beaucoup utilisé durant cette année, maitrise du design pattern MVC (model, view, controller) et donc de la programation orientée objets. Utilisation régulière du logiciel Mamp et de Phpmyadmin.\r\n\n- GitHub : Maitrise de cet outil que j’ai eu l’occasion d’utiliser de manière regulière.\r\n\n- Arduino : J’ai acquis de petite base au lycée, car mon projet au baccalauréat était la programmation d’une carte Arduino en C++.\r\n\n- MySql : Maitrise des actions de base.\r\n\n- Aws : Brève maitrise de l'environement aws grâce auquel j'ai eu l'occasion de deployer un projet en ligne.\r\n\n",
     articleImage: aquariumImage,
     blackBoard: true,
     button: false,
@@ -63,7 +63,7 @@ export const articleData = [
     title: " ",
     articleTitle: "Dans le domaine du design :",
     articleText:
-      "- Figma : Maitrise du logiciel grâce à des projets comme ce portfolio, où je m’occupe du maquettage. C’est un domaine qui me plait. J’ai aussi eu l’occasion  d’utiliser des plugins. Maitrise de l’option de prototypage sur ce logiciel. \r\n\n- Photoshop et Illustrator : Des cours de qualité durant toute l’année m’ont permis de maitriser ces logiciels.\r\n\n- Premiere pro : j’ai montée de courtes videos de maquillage artistique sur ce logiciel.",
+      "- Figma : Maitrise du logiciel grâce à des projets comme ce portfolio, pour lequel je m’occupe du maquettage. C’est un domaine qui me plait. J’ai aussi eu l’occasion d’utiliser des plugins. Maitrise de l’option de prototypage sur ce logiciel.\r\n\n- Photoshop et Illustrator : Des cours de qualité durant toute l’année m’ont permis de maitriser ces logiciels.\r\n\n- Premiere pro : j’ai monté de courtes videos de maquillages artistiques sur ce logiciel.",
     articleImage: catImage,
     blackBoard: true,
     button: false,
@@ -74,7 +74,7 @@ export const articleData = [
     title: "Des projets réalisés ? ",
     articleTitle: "Infoquiz - Concours coddity",
     articleText:
-      "Infoquiz est une application web crée pour la participation au concours boursecoddity 2020 sur la thématique de la découverte du monde de l’informatique et d’internet aux enfants.\r\n\nMon application web est une solution pour donner envie à l’enfant grâce à un design agréable et amusant de s’instruire sur le thème de l’informatique et d’internet.\r\n\nLa gamification de cette application web permettra aux enfants d’acquérir de nouvelles connaissances en plus d’avoir une certaine prévention des dangers du monde de l’informatique.",
+      "Infoquiz est une application web crée pour la participation au concours boursecoddity 2020 sur la thématique de la découverte du monde de l’informatique et d’internet aux enfants.\r\n\nMon application web est une solution pour donner envie à l’enfant grâce, à un design agréable et amusant de s’instruire sur le thème de l’informatique et d’internet.\r\n\nLa gamification de cette application web permettra aux enfants d’acquérir de nouvelles connaissances ainsi qu’une certaine prévention des dangers du monde de l’informatique.",
     articleImage: flowerImage,
     blackBoard: false,
     button: true,
@@ -86,7 +86,7 @@ export const articleData = [
     title: "",
     articleTitle: "Mon jeu vidéo fait en JavaScript",
     articleText:
-      "Durant un projet à l’école, avec une équipe, l’objectif était d’inventer un jeu vidéo sur le thème “après” entièrement codé en JavaScript (dans un délais de seulement 5 jours).\r\n\nPuis par la suite j’ai decidé de reprendre le projet et de l’améliorer. Le design à entièrement était fait par moi ainsi qu'une grande partie du code. Ce projet m’a apporté de nombreuses connaissances en JavaScript.",
+      "Durant un projet à l’école, avec une équipe, l’objectif était d’inventer un jeu vidéo sur le thème “après” entièrement codé en JavaScript (dans un délais de seulement 5 jours).\r\n\nPuis par la suite j’ai decidé de reprendre le projet et de l’améliorer. Le design a entièrement été fait par moi ainsi qu'une grande partie du code. Ce projet m’a apporté de nombreuses connaissances en JavaScript.",
     articleImage: bearImage,
     blackBoard: false,
     button: true,
@@ -97,7 +97,7 @@ export const articleData = [
     id: "",
     articleTitle: "Socoda",
     articleText:
-      "Dans le contexte d'un projet, avec une équipe, en 5 jours, nous avons du designer une interface web pour des producteurs et fournisseurs alimentaire.\r\n\nCe projet m'a fait progresser sur la gestion de projet design, sur la création d'un design système et sur l'utilisation de l'atomic design.",
+      "Dans le contexte d'un projet en une équipe, en seulement 5 jours, nous avons dû designer une interface web pour des producteurs et fournisseurs alimentaires fictifs.\r\n\nCe travail m'a fait progresser sur la gestion de projet design, sur la création d'un design système et sur l'utilisation de l'atomic design.",
     articleImage: phoneImage,
     blackBoard: false,
     button: true,
